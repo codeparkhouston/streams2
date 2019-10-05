f929a8f7b98e966b2d916ef0ad2af979930ce1fa
ironcoconut <ironcoconut@gmail.com>
2019-10-05 17:26:48 -0500

    feat: add media page at `/media`
    - page is the same as index

---
+++ src/pages/media.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Images from "../components/images"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Media" />
+    <Images />
+  </Layout>
+)
+
+export default IndexPage
