8b5049f495a0c34ebb1dc343d548d1b362f48298
Boty22 <luciabotiquin@gmail.com>
2019-10-04 22:50:53 -0500

    header color changed, layout.css font changed to Muli and Lato

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#669966`,
       marginBottom: `1.45rem`,
     }}
   >
